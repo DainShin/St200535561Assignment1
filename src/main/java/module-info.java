e780d698c002e0155c9ab04db86081173925c4c6
DainShin <di.shin0611@gmail.com>
2023-09-29 17:01:01 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.st200535561assignment1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.st200535561assignment1 to javafx.fxml;
+    exports com.example.st200535561assignment1;
+}
